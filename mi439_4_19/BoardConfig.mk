91078feb5f02d8a9231eb878396ded262c011afc
Yumi Yukimura <me.cafebabe@gmail.com>
2024-01-29 23:35:50 +0800

    mi439: k4.19: Use FBEv2+Metadata decryption
    Change-Id: Ie0abb90cf47ac075b8e374ee23d3e6d2e3ce2fc9

--- mi439_4_19/BoardConfig.mk
+++ mi439_4_19/BoardConfig.mk
@@ -1,2 +1,5 @@
 # Inherit from upper BoardConfig.mk
 include device/xiaomi/mi439/BoardConfig.mk
+
+# Properties
+TARGET_VENDOR_PROP += device/xiaomi/mi439/mi439_4_19/vendor.prop
--- recovery/root/system/bin/merge-fstab.sh
+++ recovery/root/system/bin/merge-fstab.sh
@@ -1,9 +1,22 @@
 #!/system/bin/sh
 
+if grep -F "Linux version 4.19" /proc/version > /dev/null; then
+    KERNEL_VERSION="4.19"
+fi
+
 echo >> /system/etc/recovery.fstab
 cat /system/etc/recovery.fstab.device >> /system/etc/recovery.fstab
 echo >> /system/etc/twrp.flags
 cat /system/etc/twrp.flags.device >> /system/etc/twrp.flags
+
+# Decryption
+if [ "$KERNEL_VERSION" == "4.19" ]; then
+    cat /system/etc/recovery.fstab.fbev2 >> /system/etc/recovery.fstab
+    cat /system/etc/twrp.flags.fbev2 >> /system/etc/twrp.flags
+else
+    cat /system/etc/recovery.fstab.fbev1 >> /system/etc/recovery.fstab
+    cat /system/etc/twrp.flags.fbev1 >> /system/etc/twrp.flags
+fi
 
 # Dynamic Partitions
 if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep mi439_dynpart > /dev/null; then
